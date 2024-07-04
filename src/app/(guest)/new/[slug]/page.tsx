51521fabf5e3833a15f976c6a0b2bd9678d29664
Hongsonnguyen <hongsonnguyen1003@gmail.com>
2024-07-04 17:44:54 +0700

    remove /login /news

--- src/app/(guest)/new/[slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-import NewPage from '@/containers/NewPage';
-
-const DetailNewPage = ({ params }: { params: { slug: string } }) => {
-  const slug = params.slug;
-  return <NewPage slug={slug} />;
-};
-export default DetailNewPage;
